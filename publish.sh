18d954c3b37e35a247aeaea7b3eea227d2fc1949
flex-juwon <juwon@flex.team>
2023-11-05 11:37:00 +0900

    jekyll start guide at publish.sh

--- publish.sh
+++ publish.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
+# INSTALL GUIDE
+#
+# brew install ruby
+# export PATH="$(brew --prefix ruby)/bin:$PATH"
+#
+# mkdir -p $HOME/gems
+# export GEM_HOME=$HOME/gems
+# export GEM_PATH=$HOME/gems
+# export PATH=$GEM_HOME/bin:$PATH
+#
+# gem install bundler jekyll jekyll-paginate
+
 git subtree push --prefix public origin gh-pages
